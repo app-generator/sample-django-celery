1a01826b4775ce1eb7b5269724d04d685cac9ba7
App Generator <51070104+app-generator@users.noreply.github.com>
2022-10-03 20:56:43 +0300

    Integrate Tasks APP

--- core/urls.py
+++ core/urls.py
@@ -7,10 +7,11 @@ from django.contrib import admin
 from django.urls import path, include  # add this
 
 urlpatterns = [
-    path('admin/', admin.site.urls),          # Django admin route
+    path('admin/', admin.site.urls),               # Django admin route
     path("", include("apps.authentication.urls")), # Auth routes - login / register
 
     # ADD NEW Routes HERE
+    path("", include("apps.tasks.urls")),          # Tasks Routes
 
     # Leave `Home.Urls` as last the last line
     path("", include("apps.home.urls"))
