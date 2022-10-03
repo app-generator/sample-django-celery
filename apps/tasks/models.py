84f866ab2d2e1ca2cbf1e9aa34b06f6a97442151
App Generator <51070104+app-generator@users.noreply.github.com>
2022-10-03 21:04:00 +0300

    Tasks Models - Added Definitions

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -4,7 +4,24 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
 
+'''
+Tasks: 
+- id (PK), 
+- name,         # user input  
+- script_name,  # Dynamic list -> celery_scripts
+- description   # user input
+'''
+
+'''
+TaskStatus: 
+- id (PK),       
+- task_id,      # linked to tasks.ID
+- ts_start,     # Unix timestamp (seconds)
+- ts_end,       # Unix timestamp (seconds)
+- state:        # RUNNING, FINISHED, CANCELED, UNKNOWN 
+- status:       # OK, FAILED, UNKNOWN 
+- log_file      # None or file saved in celery_logs DIRECTORY
+'''
