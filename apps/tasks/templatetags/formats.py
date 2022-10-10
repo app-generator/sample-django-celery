73c0758881b1bfcf596c7f778639ca7d3785fcc8
App Generator <51070104+app-generator@users.noreply.github.com>
2022-10-10 07:47:26 +0300

    Fix date_format() - bumps runtime ERR

--- apps/tasks/templatetags/formats.py
+++ apps/tasks/templatetags/formats.py
@@ -3,7 +3,9 @@ from django import template
 register = template.Library()
 
 def date_format(date):
-    return  date.strftime(r'%Y-%m-%d-%H-%M-%S')
-
+    try:
+        return  date.strftime(r'%Y-%m-%d-%H-%M-%S')
+    except:
+        return date
 
 register.filter("date_format", date_format)
