bf7d8eb287b2dadfe02abd39592fe7b5d43605ab
App Generator <51070104+app-generator@users.noreply.github.com>
2022-10-10 09:33:02 +0300

    CleanUP

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -7,21 +7,3 @@ from django.db import models
 
 # Create your models here.
 
-'''
-Tasks: 
-- id (PK), 
-- name,         # user input  
-- script_name,  # Dynamic list -> celery_scripts
-- description   # user input
-'''
-
-'''
-TaskStatus: 
-- id (PK),       
-- task_id,      # linked to tasks.ID
-- ts_start,     # Unix timestamp (seconds)
-- ts_end,       # Unix timestamp (seconds)
-- state:        # RUNNING, FINISHED, CANCELED, UNKNOWN 
-- status:       # OK, FAILED, UNKNOWN 
-- log_file      # None or file saved in celery_logs DIRECTORY
-'''
