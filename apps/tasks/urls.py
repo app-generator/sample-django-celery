579eaeca52b29bc78c2a2db469d7ddaaa04dd196
App Generator <51070104+app-generator@users.noreply.github.com>
2022-10-11 09:53:29 +0300

    Tasks - New routing (View LOG)

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('tasks', views.tasks, name="tasks"),
     path('tasks/run/<str:task_name>', views.run_task, name="run-task"),
     path('tasks/cancel/<str:task_id>', views.cancel_task, name="cancel-task"),
+    path('tasks/output/<str:task_id>', views.task_output, name="task-output"),
+    path('tasks/log/<str:task_id>'   , views.task_log, name="task-log"),
 ]
