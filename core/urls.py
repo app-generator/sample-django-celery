c789cf28b6e24cf5bd4f1beaa91a4ca09d8c5d63
App Generator <51070104+app-generator@users.noreply.github.com>
2022-10-12 16:28:25 +0300

    Update Path for Celery TM

--- core/urls.py
+++ core/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path("", include("apps.authentication.urls")), # Auth routes - login / register
 
     # ADD NEW Routes HERE
-    path("", include("apps.tasks.urls")),          # Tasks Routes
+    path("", include("django_tm.urls")),           # Tasks Routes
 
     # Leave `Home.Urls` as last the last line
     path("", include("apps.home.urls"))
